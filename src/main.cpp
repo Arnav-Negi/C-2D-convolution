6a3d6c83f6b091499323d528aa05e2a22f02a5b2
Arnav-Negi <arnav.negi@students.iiit.ac.in>
2024-04-07 01:11:07 +0530

    kernel specific

--- src/main.cpp
+++ src/main.cpp
@@ -14,29 +14,21 @@ namespace solution {
     std::string compute(const std::string &bitmap_path, const float kernel[3][3], const std::int32_t num_rows,
                         const std::int32_t num_cols) {
         std::string sol_path = std::filesystem::temp_directory_path() / "student_sol.bmp";
-//        std::ofstream sol_fs(sol_path, std::ios::binary);
-//        std::ifstream bitmap_fs(bitmap_path, std::ios::binary);
 
         const float kernel1d[3] = {0.25f, 0.5f, 0.25f};
 
-//        const auto img = std::make_unique<float[]>(num_rows * num_cols);
-//        const auto padded_img = std::make_unique<float[]>((num_rows + 2) * (num_cols + 2));
         // try raw pointer
         auto *padded_img = static_cast<float *>(malloc((num_rows + 2) * (num_cols + 2) * sizeof(float)));
         auto *output_img = static_cast<float *>(malloc((num_rows + 2) * (num_cols + 2) * sizeof(float)));
-//        const auto horizontal_conv_img = std::make_unique<float[]>((num_rows + 2) * (num_cols + 2));
-//        bitmap_fs.read(reinterpret_cast<char *>(img.get()), sizeof(float) * num_rows * num_cols);
 
         std::FILE *file = std::fopen(bitmap_path.c_str(), "rb");
         // Padding
         for (std::int32_t i = 0; i < num_rows; i++) {
-//            bitmap_fs.read(reinterpret_cast<char *>(padded_img + (i + 1) * (num_cols + 2) + 1),
-//                           sizeof(float) * num_cols);
-// use fread
+        // use fread
             std::fread(padded_img + (i + 1) * (num_cols + 2) + 1, sizeof(float), num_cols, file);
         }
         std::fclose(file);
-//        bitmap_fs.close();
+
         // pad with zeros
         for (std::int32_t i = 0; i < num_rows + 2; i++) {
             padded_img[i * (num_cols + 2)] = 0.0;
@@ -47,37 +39,65 @@ namespace solution {
             padded_img[(num_rows + 1) * (num_cols + 2) + j] = 0.0;
         }
 
-        __m512 kernel_vec[3][3];
+//        __m512 kernel_vec[3][3];
+//        for (std::int32_t i = 0; i < 3; i++) {
+//            for (std::int32_t j = 0; j < 3; j++) {
+//                kernel_vec[i][j] = _mm512_set1_ps(kernel[i][j]);
+//            }
+//        }
+//#pragma omp parallel for collapse(1) schedule(static) num_threads(16) shared(padded_img, output_img, kernel_vec)
+//        for (std::int32_t i = 1; i < num_rows + 1; i++) {
+//            for (std::int32_t j = 1; j < num_cols + 1; j += 16) {
+//                __m512 sum = _mm512_setzero_ps();
+//                for (std::int32_t di = -1; di <= 1; di++) {
+//                    for (std::int32_t dj = -1; dj <= 1; dj++) {
+//                        __m512 img_val = _mm512_loadu_ps(padded_img + (i + di) * (num_cols + 2) + j + dj);
+//                        sum = _mm512_fmadd_ps(kernel_vec[di + 1][dj + 1], img_val, sum);
+//                    }
+//                }
+//                // store the sum
+//                _mm512_storeu_ps(output_img + i * (num_cols + 2) + j, sum);
+//            }
+//        }
+
+        __m512 kernel_vec[3];
         for (std::int32_t i = 0; i < 3; i++) {
-            for (std::int32_t j = 0; j < 3; j++) {
-                kernel_vec[i][j] = _mm512_set1_ps(kernel[i][j]);
-            }
+            kernel_vec[i] = _mm512_set1_ps(kernel1d[i]);
         }
-        std::FILE *sol_fs = std::fopen(sol_path.c_str(), "wb");
+
+        // horizontal pass
 #pragma omp parallel for collapse(1) schedule(static) num_threads(16) shared(padded_img, output_img, kernel_vec)
         for (std::int32_t i = 1; i < num_rows + 1; i++) {
-            for (std::int32_t j = 1; j < num_cols + 1; j += 16) {
+            for (std::int32_t j = 1; j < num_cols + 1; j+=16) {
                 __m512 sum = _mm512_setzero_ps();
-                for (std::int32_t di = -1; di <= 1; di++) {
-                    for (std::int32_t dj = -1; dj <= 1; dj++) {
-//                            sum += kernel[di + 1][dj + 1] * padded_img[(i + di) * (num_cols + 2) + j + dj];
-                        __m512 img_val = _mm512_loadu_ps(padded_img + (i + di) * (num_cols + 2) + j + dj);
-                        sum = _mm512_fmadd_ps(kernel_vec[di + 1][dj + 1], img_val, sum);
-                    }
+                for (std::int32_t dj = -1; dj <= 1; dj++) {
+                    __m512 img_val = _mm512_loadu_ps(padded_img + i * (num_cols + 2) + j + dj);
+                    sum = _mm512_fmadd_ps(kernel_vec[dj+1], img_val, sum);
                 }
-                // store the sum
-//                sol_fs.write(reinterpret_cast<char *>(&sum), sizeof(sum));
                 _mm512_storeu_ps(output_img + i * (num_cols + 2) + j, sum);
             }
         }
 
+#pragma omp parallel for collapse(1) schedule(static) num_threads(16) shared(padded_img, output_img, kernel_vec)
+        // vertical pass - from output_img to padded_img
+        for (std::int32_t i = 1; i < num_rows + 1; i++) {
+            for (std::int32_t j = 1; j < num_cols + 1; j+=16) {
+                __m512 sum = _mm512_setzero_ps();
+                for (std::int32_t di = -1; di <= 1; di++) {
+                    __m512 img_val = _mm512_loadu_ps(output_img + (i + di) * (num_cols + 2) + j);
+                    sum = _mm512_fmadd_ps(kernel_vec[di+1], img_val, sum);
+                }
+//                store in padded_img without padding
+                _mm512_storeu_ps(padded_img + (i - 1) * num_cols + j-1, sum);
+            }
+        }
+
+        std::FILE *sol_fs = std::fopen(sol_path.c_str(), "wb");
         // write the output image
         for (std::int32_t i = 1; i < num_rows + 1; i++) {
-//            sol_fs.write(reinterpret_cast<char *>(output_img + i * (num_cols + 2) + 1), sizeof(float) * num_cols);
             std::fwrite(output_img + i * (num_cols + 2) + 1, sizeof(float), num_cols, sol_fs);
         }
 
-//        sol_fs.close();
         std::fclose(sol_fs);
         free(padded_img);
         free(output_img);

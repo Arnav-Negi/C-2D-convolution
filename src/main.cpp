95bb52c730eb71354e7ddc7a4818427e5865623e
Arnav-Negi <arnav.negi@students.iiit.ac.in>
2024-04-09 03:16:58 +0530

    fixed bug

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,6 @@ namespace solution {
         std::string sol_path = std::filesystem::temp_directory_path() / "student_sol.bmp";
 
         constexpr std::int32_t VEC_SIZE = 16;
-        constexpr std::int32_t BLOCK_SIZE = 64;
         constexpr std::int32_t NUM_THREADS = 4;
 
 
@@ -49,8 +48,7 @@ namespace solution {
             }
         }
 
-#pragma omp parallel num_threads(NUM_THREADS) default(none) shared(input_img, output_img) \
-firstprivate(kernel, kernel_vec, kernel_vec8, num_cols, num_rows)
+#pragma omp parallel num_threads(NUM_THREADS) default(none) shared(input_img, output_img) firstprivate(kernel, kernel_vec, kernel_vec8, num_cols, num_rows)
         {
 
             // all corners along with 8 pixels along each side
@@ -58,18 +56,19 @@ firstprivate(kernel, kernel_vec, kernel_vec8, num_cols, num_rows)
             {
                 // non vectorized code for corners
                 output_img[0] = kernel[1][1] * input_img[0] + kernel[1][2] * input_img[1] +
-                                   kernel[2][1] * input_img[num_cols] + kernel[2][2] * input_img[num_cols + 1];
-                output_img[num_cols - 1] = kernel[1][0] * input_img[num_cols - 2] + kernel[1][1] * input_img[num_cols - 1] +
-                                               kernel[2][0] * input_img[2 * num_cols - 2] +
-                                               kernel[2][1] * input_img[2 * num_cols - 1];
+                                kernel[2][1] * input_img[num_cols] + kernel[2][2] * input_img[num_cols + 1];
+                output_img[num_cols - 1] =
+                        kernel[1][0] * input_img[num_cols - 2] + kernel[1][1] * input_img[num_cols - 1] +
+                        kernel[2][0] * input_img[2 * num_cols - 2] +
+                        kernel[2][1] * input_img[2 * num_cols - 1];
                 output_img[num_cols * (num_rows - 1)] = kernel[0][1] * input_img[num_cols * (num_rows - 2)] +
-                                                         kernel[0][2] * input_img[num_cols * (num_rows - 2) + 1] +
-                                                         kernel[1][1] * input_img[num_cols * (num_rows - 1)] +
-                                                         kernel[1][2] * input_img[num_cols * (num_rows - 1) + 1];
+                                                        kernel[0][2] * input_img[num_cols * (num_rows - 2) + 1] +
+                                                        kernel[1][1] * input_img[num_cols * (num_rows - 1)] +
+                                                        kernel[1][2] * input_img[num_cols * (num_rows - 1) + 1];
                 output_img[num_cols * num_rows - 1] = kernel[0][0] * input_img[num_cols * (num_rows - 1) - 2] +
-                                                        kernel[0][1] * input_img[num_cols * (num_rows - 1) - 1] +
-                                                        kernel[1][0] * input_img[num_cols * num_rows - 2] +
-                                                        kernel[1][1] * input_img[num_cols * num_rows - 1];
+                                                      kernel[0][1] * input_img[num_cols * (num_rows - 1) - 1] +
+                                                      kernel[1][0] * input_img[num_cols * num_rows - 2] +
+                                                      kernel[1][1] * input_img[num_cols * num_rows - 1];
             }
 
             // top row
@@ -88,7 +87,7 @@ firstprivate(kernel, kernel_vec, kernel_vec8, num_cols, num_rows)
                     for (std::int32_t di = 0; di <= 1; di++) {
                         for (std::int32_t dj = -1; dj <= 1; dj++) {
                             __m512 img_val = _mm512_loadu_ps(input_img + di * num_cols + j + dj);
-                            sum = _mm512_fmadd_ps(kernel_vec[di][dj + 1], img_val, sum);
+                            sum = _mm512_fmadd_ps(kernel_vec[di + 1][dj + 1], img_val, sum);
                         }
                     }
 
@@ -102,12 +101,13 @@ firstprivate(kernel, kernel_vec, kernel_vec8, num_cols, num_rows)
             {
                 // start from 1 to 14 no vectorize
                 for (std::int32_t j = 1; j < 15; j++) {
-                    output_img[(num_rows - 1) * num_cols + j] = kernel[0][0] * input_img[(num_rows - 2) * num_cols + j - 1] +
-                                                               kernel[0][1] * input_img[(num_rows - 2) * num_cols + j] +
-                                                               kernel[0][2] * input_img[(num_rows - 2) * num_cols + j + 1] +
-                                                               kernel[1][0] * input_img[(num_rows - 1) * num_cols + j - 1] +
-                                                               kernel[1][1] * input_img[(num_rows - 1) * num_cols + j] +
-                                                               kernel[1][2] * input_img[(num_rows - 1) * num_cols + j + 1];
+                    output_img[(num_rows - 1) * num_cols + j] =
+                            kernel[0][0] * input_img[(num_rows - 2) * num_cols + j - 1] +
+                            kernel[0][1] * input_img[(num_rows - 2) * num_cols + j] +
+                            kernel[0][2] * input_img[(num_rows - 2) * num_cols + j + 1] +
+                            kernel[1][0] * input_img[(num_rows - 1) * num_cols + j - 1] +
+                            kernel[1][1] * input_img[(num_rows - 1) * num_cols + j] +
+                            kernel[1][2] * input_img[(num_rows - 1) * num_cols + j + 1];
                 }
                 // need to start from 15 and go till num_cols - 2
                 for (std::int32_t j = 15; j < num_cols - 1; j += VEC_SIZE) {
@@ -128,12 +128,13 @@ firstprivate(kernel, kernel_vec, kernel_vec8, num_cols, num_rows)
 #pragma omp single nowait
             {
                 for (std::int32_t i = 1; i < num_rows - 1; i++) { // 6 multiply and adds
-                    output_img[i * num_cols + num_cols - 1] = kernel[0][0]*input_img[(i-1)*num_cols + num_cols - 2] +
-                                                             kernel[0][1]*input_img[(i-1)*num_cols + num_cols - 1] +
-                                                             kernel[1][0]*input_img[i*num_cols + num_cols - 2] +
-                                                             kernel[1][1]*input_img[i*num_cols + num_cols - 1] +
-                                                             kernel[2][0]*input_img[(i+1)*num_cols + num_cols - 2] +
-                                                             kernel[2][1]*input_img[(i+1)*num_cols + num_cols - 1];
+                    output_img[i * num_cols + num_cols - 1] =
+                            kernel[0][0] * input_img[(i - 1) * num_cols + num_cols - 2] +
+                            kernel[0][1] * input_img[(i - 1) * num_cols + num_cols - 1] +
+                            kernel[1][0] * input_img[i * num_cols + num_cols - 2] +
+                            kernel[1][1] * input_img[i * num_cols + num_cols - 1] +
+                            kernel[2][0] * input_img[(i + 1) * num_cols + num_cols - 2] +
+                            kernel[2][1] * input_img[(i + 1) * num_cols + num_cols - 1];
                 }
             };
 
@@ -141,23 +142,24 @@ firstprivate(kernel, kernel_vec, kernel_vec8, num_cols, num_rows)
 #pragma omp single nowait
             {
                 for (std::int32_t i = 1; i < num_rows - 1; i++) { // 6 multiply and adds
-                    output_img[i * num_cols] = kernel[0][1]*input_img[(i-1)*num_cols] +
-                                               kernel[0][2]*input_img[(i-1)*num_cols + 1] +
-                                               kernel[1][1]*input_img[i*num_cols] +
-                                               kernel[1][2]*input_img[i*num_cols + 1] +
-                                               kernel[2][1]*input_img[(i+1)*num_cols] +
-                                               kernel[2][2]*input_img[(i+1)*num_cols + 1];
+                    output_img[i * num_cols] = kernel[0][1] * input_img[(i - 1) * num_cols] +
+                                               kernel[0][2] * input_img[(i - 1) * num_cols + 1] +
+                                               kernel[1][1] * input_img[i * num_cols] +
+                                               kernel[1][2] * input_img[i * num_cols + 1] +
+                                               kernel[2][1] * input_img[(i + 1) * num_cols] +
+                                               kernel[2][2] * input_img[(i + 1) * num_cols + 1];
                 }
             };
 
             // for each row 1 to num_rows - 2, column 1 to 14
 #pragma omp for schedule(static) collapse(1) nowait
-            for (int i = 1; i < num_rows - 1 ; ++i) {
+            for (int i = 1; i < num_rows - 1; ++i) {
                 // start from 1 to 6, no vectorize
                 for (int j = 1; j <= 6; ++j) {
                     for (int di = -1; di <= 1; ++di) {
                         for (int dj = -1; dj <= 1; ++dj) {
-                            output_img[i * num_cols + j] += kernel[di + 1][dj + 1] * input_img[(i + di) * num_cols + j + dj];
+                            output_img[i * num_cols + j] +=
+                                    kernel[di + 1][dj + 1] * input_img[(i + di) * num_cols + j + dj];
                         }
                     }
                 }
@@ -167,7 +169,7 @@ firstprivate(kernel, kernel_vec, kernel_vec8, num_cols, num_rows)
                 for (int di = -1; di <= 1; ++di) {
                     for (int dj = -1; dj <= 1; ++dj) {
                         __m256 img_val = _mm256_loadu_ps(input_img + (i + di) * num_cols + 7 + dj);
-                        sum = _mm256_fmadd_ps(kernel_vec8[di+1][dj+1], img_val, sum);
+                        sum = _mm256_fmadd_ps(kernel_vec8[di + 1][dj + 1], img_val, sum);
                     }
                 }
             }
@@ -188,7 +190,7 @@ firstprivate(kernel, kernel_vec, kernel_vec8, num_cols, num_rows)
                     _mm512_storeu_ps(output_img + i * num_cols + j, sum);
                 }
             }
-
+# pragma omp barrier
         }
 
 //    std::cout << "convolution successful" << std::endl;
